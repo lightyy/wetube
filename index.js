7a1644519950de30be64d1e8f6f32b121115fba1
lightyy <lightyy22@gmail.com>
2020-06-17 23:35:08 +0900

    add middleware

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
 // const express = require("express");
 import express from "express";
+import morgan from "morgan"; //logger
+import helmet from "helmet"; //보안
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+
 const app = express();
 const PORT = 4000;
 
 const handleListening = () => {
   console.log(`Listening on: http://localhost:${PORT}`);
-}
+};
 const handleHome = (req, res) => {
   //   console.log(req);
+  console.log("I'm Home");
   res.send("Hello from home!!!!!!!!!!!!!");
 };
 
-const handleProfile = (req, res) => res.send("You are on my profile");
+const handleProfile = (req, res) => {
+  console.log("I'm profile");
+  res.send("You are on my profile");
+};
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan("dev"));
 
 app.get("/", handleHome);
-
 app.get("/profile", handleProfile);
-
 app.listen(PORT, handleListening);
