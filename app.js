e27d2fa9f10dc47d4778702b4f5024304953944e
lightyy <lightyy22@gmail.com>
2020-06-18 00:27:43 +0900

    Router

--- app.js
+++ app.js
@@ -4,13 +4,9 @@ import morgan from "morgan"; //logger
 import helmet from "helmet"; //보안
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-
+import { userRouter } from "./router";
 const app = express();
-const PORT = 4000;
 
-const handleListening = () => {
-  console.log(`Listening on: http://localhost:${PORT}`);
-};
 const handleHome = (req, res) => {
   //   console.log(req);
   console.log("I'm Home");
@@ -29,5 +25,9 @@ app.use(helmet());
 app.use(morgan("dev"));
 
 app.get("/", handleHome);
+
 app.get("/profile", handleProfile);
-app.listen(PORT, handleListening);
+
+app.use("/user", userRouter);
+
+export default app;
