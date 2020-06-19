58d8a838b7f74855560e584e7fbd4d19a89ac70c
lightyy <lightyy22@gmail.com>
2020-06-20 02:03:41 +0900

    mvc

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,5 +1,12 @@
 import express from "express";
+import routes from "../routes";
+import app from "../app";
 
 const userRouter = express.Router();
+
+userRouter.get(routes.home, (req, res) => res.send("Users"));
+userRouter.get(routes.userDetail, (req, res) => res.send("userDetail"));
+userRouter.get(routes.editProfile, (req, res) => res.send("editProfile"));
+userRouter.get(routes.changePassword, (req, res) => res.send("changePassword"));
 
 export default userRouter;
