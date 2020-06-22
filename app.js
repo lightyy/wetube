c2718a891d94c27af870be3d359f693e04f125e2
lightyy <lightyy22@gmail.com>
2020-06-23 01:01:28 +0900

    pug

--- app.js
+++ app.js
@@ -4,19 +4,23 @@ import morgan from "morgan"; //logger
 import helmet from "helmet"; //보안
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import { localMiddleware } from "./middlewares";
 //Router
+import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
-import globalRouter from "./routers/globalRouter";
-import routes from "./routes"
+import routes from "./routes";
 
 const app = express();
 
+app.use(helmet());
+app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(helmet());
 app.use(morgan("dev"));
+//전역변수 추가
+app.use(localMiddleware);
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
