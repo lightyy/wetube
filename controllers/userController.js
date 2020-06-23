f95700e1e47a7ae914c14d91f204dd250cfdbaa3
lightyy <lightyy22@gmail.com>
2020-06-23 14:31:51 +0900

    searching controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,7 @@
-export const join = (req, res) => res.send("Join");
-export const login = (req, res) => res.send("Login");
-export const logout = (req, res) => res.send("Logout");
+export const join = (req, res) => res.render("join", { pageTitle: "Join" });
+export const login = (req, res) => res.render("login", { pageTitle: "Login" });
+export const logout = (req, res) => res.render("logout", { pageTitle: "Logout" });
+export const users = (req, res) => res.render("users", { pageTitle: "users" });
+export const userDetail = (req, res) => res.render("userDetail", { pageTitle: "userDetail" });
+export const editProfile = (req, res) => res.render("editProfile", { pageTitle: "editProfile" });
+export const changePassword = (req, res) => res.render("changePassword", { pageTitle: "changePassword" });
--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,2 +1,23 @@
-export const home = (req, res) => res.send("Home");
-export const search = (req, res) => res.send("Search");
+export const home = (req, res) => res.render("home", { pageTitle: "home" });
+export const search = (req, res) => {
+  //   const searchingBy = req.query.term;
+  const {
+    query: { term: searchingBy },
+  } = req;
+  //   console.log(term);
+  res.render("search", { pageTitle: "search", searchingBy: searchingBy });
+};
+export const videos = (req, res) =>
+  res.render("videos", { pageTitle: "videos" });
+
+export const upload = (req, res) =>
+  res.render("upload", { pageTitle: "upload" });
+
+export const videoDetail = (req, res) =>
+  res.render("videoDetail", { pageTitle: "videoDetail" });
+
+export const editVideo = (req, res) =>
+  res.render("editVideo", { pageTitle: "editVideo" });
+
+export const deleteVideo = (req, res) =>
+  res.render("deleteVideo", { pageTitle: "deleteVideo" });
